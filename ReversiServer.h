53d087737ddc1cac5fd3cc782b0a684d9e3f7d22
Yahel <yahel.ben.ishay@gmail.com>
2017-12-07 17:07:50 +0200

    Server project startup

---
+++ ReversiServer.h
@@ -0,0 +1,23 @@
+//
+// Created by sagi shaashua on 07/12/2017.
+//
+
+#ifndef SERVER_REVERSISERVER_H
+#define SERVER_REVERSISERVER_H
+
+
+class ReversiServer {
+public:
+    ReversiServer(int port);
+    void start();
+    void stop();
+private:
+    int port;
+    int serverSocket;
+
+    void hendleClient(int clientServer);
+
+};
+
+
+#endif //SERVER_REVERSISERVER_H
