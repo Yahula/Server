0b90de9045663c0de4a5a40ffb7ffb555b70bba4
Sagi <sagishaashua@Gmail.com>
2017-12-26 21:02:27 +0200

    players working

--- ReversiServer.h
+++ ReversiServer.h
@@ -5,18 +5,58 @@
 #ifndef SERVER_REVERSISERVER_H
 #define SERVER_REVERSISERVER_H
 
-
+/**
+ * this class is our server. it conecctes players to play a game.
+ */
 class ReversiServer {
+
 public:
+    /**
+     * the constructor.
+     * @param port the number of the port.
+     */
     ReversiServer(int port);
+
+    /**
+     * starts the connection.
+     */
     void start();
+    /**
+     * ends the connection.
+     */
     void stop();
+
 private:
+    /**
+     * the number of the port.
+     */
     int port;
+
+    /**
+     *
+     */
     int serverSocket;
+
+    /**
+     * the command to pass between the players. what move did the other player make.
+     */
     char* move;
+
+    /**
+     * the function that controls one comunication cercal with one player.
+     * it tells him what was the previews move and ask for the new move.
+     * @param clientServer which player.
+     */
     void hendleClient(int clientServer);
+
+    /**
+     * counts how many times in a raw the players couldn't move.
+     */
     int noMovesCounter;
+
+    /**
+     * an array of players(clients).
+     */
     int* clients;
 };
 
