b16ad7f8214850587ee3c93f97015b3e81af0f33
Sagi <sagishaashua@Gmail.com>
2018-01-07 18:32:12 +0200

    list of games! boyyaaa

--- src/Server/ListGamesCommand.cpp
+++ src/Server/ListGamesCommand.cpp
@@ -14,14 +14,13 @@ ListGamesCommand::ListGamesCommand(vector<NetworkGame *> *gamesList) {
 void ListGamesCommand::execute(vector<string> args, pthread_t threadId, int socket) {
 
     for(int i =0; i< gamesList->size() ; i++ ){
-        string s = gamesList->at(i)->getName();
+        string s = gamesList->at(i)->getName()+" \n";
         int w = write(socket, &s, s.size());
         if (w == -1) {
             std::cout << "Error writing to client" << std::endl;
             return;
         }
     }
-    cout<< gamesList->at(0)->getName() <<endl;
     char msg[] = "END_LIST";
     int w = write(socket, msg, strlen(msg));
     if (w == -1) {
